1e5077b1cbe111946a283a8b6dcde23e9f78025f
mounika-nannapaneni <mounika.nannapaneni08@gmail.com>
2026-01-22 11:57:14 +0530

    Update revenue_analysis.sql

--- analysis/revenue_analysis.sql
+++ analysis/revenue_analysis.sql
@@ -1,6 +1,6 @@
 
 -- This query calculates the total revenue generated 
--- by summing the product of quantity and item priceacross all order items.
+-- by summing the product of quantity and item price across all order items.
 
 SELECT 
     SUM(quantity * item_price) AS total_revenue
